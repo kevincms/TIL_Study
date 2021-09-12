4d34d81abea34e3dfcf37a42bf2521e574d466e9
kevincms314 <kevincms314@naver.com>
2021-09-12 23:49:40 +0900

    2021-09-12 Python Basic Proceed Day 2

--- Python/Python_basic/1. print.py
+++ Python/Python_basic/1. print.py
@@ -1,15 +1,7 @@
-language=["Python","C","Java","C++","Go"]
+language="python"
 language_number=1
 
-# +로 구분할 때에는 모두 문자열(str)이어야 한다.
-print(str(language_number)+". "+"Hello "+language[language_number-1])
-language_number+=1
+print(str(language_number)+". "+"Hello "+language)
 
 # +가 아닌 ,로 구분가능 (단 띄어쓰기가 됨) (혼용 가능)
-print(language_number,".","Hello ",language[language_number-1])
-language_number+=1
-
-# 파이썬은 자동적으로 줄바꿈이 된다. (end를 이용하면 뒤의 출력을 바꿀 수 있다.))
-print(str(language_number)+". "+"Hello ",end="")
-print(language[language_number-1])
-language_number+=1
+print(language_number,".","Hello ",language)
---
+++ Python/Python_basic/3.2. str_function.py
@@ -0,0 +1,5 @@
+count=1
+print("Hello World",len("Hello World")) # 문자열 길이 반환
+count+=1
+print("Hello World".replace("World","Python")) # 문자열 변경
+count+=1
