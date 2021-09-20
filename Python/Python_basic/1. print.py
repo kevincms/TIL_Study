840a0c99139555c09e9503013f97ba6a7d24df00
kevincms <kevincms314@naver.com>
2021-09-20 21:58:30 +0900

    2021-09-20 Python Basic Proceed Day 10

--- Python/Python_basic/1. print.py
+++ Python/Python_basic/1. print.py
@@ -1,35 +1,35 @@
 language=["Python","C","Java","C++","Go","JavaScript","Swift","C#"]
-language_number=1
+count=1
 
 # +로 구분할 때에는 모두 문자열(str)이어야 한다.
-print(str(language_number)+". "+"Hello "+language[language_number-1]) # 1.
-language_number+=1
+print(str(count)+". "+"Hello "+language[count-1]) # 1.
+count+=1
 
 # +가 아닌 ,로 구분가능 (단 띄어쓰기가 됨, sep으로 수정가능) (혼용 가능)
-print(language_number,".","Hello ",language[language_number-1]) # 2. 
-language_number+=1
+print(count,".","Hello ",language[count-1]) # 2. 
+count+=1
 
 # 파이썬은 자동적으로 줄바꿈이 된다. (end를 이용하면 뒤의 출력을 바꿀 수 있다.)
-print(str(language_number),". ","Hello ",end="",sep="") # 3.
-print(language[language_number-1])
-language_number+=1
+print(str(count),". ","Hello ",end="",sep="") # 3.
+print(language[count-1])
+count+=1
 
 # 다음과 같이 변수를 출력할 수 있다.
-print("%d. Hello %s" % (language_number, language[language_number-1])) # 4. %d int | %f, %lf float | %s str
-language_number+=1
+print("%d. Hello %s" % (count, language[count-1])) # 4. %d int | %f, %lf float | %s str
+count+=1
 
-print("{}. Hello {}".format(language_number,language[language_number-1])) # 5.
-language_number+=1
+print("{}. Hello {}".format(count,language[count-1])) # 5.
+count+=1
 
-print("{1}. Hello {0}".format(language[language_number-1],language_number)) # 6.
-language_number+=1
+print("{1}. Hello {0}".format(language[count-1],count)) # 6.
+count+=1
 
-print("{num}. Hello {Contents}".format(num=language_number,Contents=language[language_number-1])) # 7.
-language_number+=1
+print("{num}. Hello {Contents}".format(num=count,Contents=language[count-1])) # 7.
+count+=1
 
-print(f"{language_number}. Hello {language[language_number-1]}") # 8. ver 3.6 이상만 된다.
-language_number+=1
+print(f"{count}. Hello {language[count-1]}") # 8. ver 3.6 이상만 된다.
+count+=1
 
 very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_long_variable="PYTHON"
 print("{}. very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_very_long_variable"\
-    .format(language_number)) # 9. \(역슬래쉬)로 줄을 바꿔도 한줄로 인식할수 있다.
+    .format(count)) # 9. \(역슬래쉬)로 줄을 바꿔도 한줄로 인식할수 있다.
--- Python/Python_basic/9. class.py
+++ Python/Python_basic/9. class.py
@@ -1,15 +1,54 @@
+language=["Python","C","Java","C++","Go","JavaScript","Swift","C#"]
 count=1
     
-class test_class:
+class test_language:
     def __init__(self,count): # __init__ : 생성자 (클래스가 생성될 때 실행되는 함수)
         self.count=count # self : class 내에서 정의된 변수
-    def test_print(self): # class의 함수에서는 아무런 변수를 입력받지 않더라도 self를 사용해야 함
+    def count_print(self): # class의 함수에서는 아무런 변수를 입력받지 않더라도 self를 사용해야 함
         print("%d."%self.count,end=" ")
-    def test_language(self):
+    def language_print(self):
         print(self.language)
-        
 
-test_class_variable=test_class(count)
-test_class_variable.test_print() # 1. class 변수(객체) 생성 후 출력
-test_class_variable.language="python" # 외부에서 변수를 만든 후 사용할 수 있다. (변수를 만든 객체에만 생성됨. )
-test_class_variable.test_language()
+test_class_variable=test_language(count)
+test_class_variable.count_print() # 1. class 변수(객체) 생성 후 출력
+test_class_variable.language=language[count-1] # 외부에서 변수를 만든 후 사용할 수 있다. (변수를 만든 객체에만 생성됨)
+test_class_variable.language_print()
+count+=1
+
+class language_characteristic:
+    def __init__(self,language):
+        self.language=language
+    def characteristic_print(self):
+        if self.language=="C":
+            print("C는 절차지향 언어이다.")
+        elif self.language=="Java":
+            print("Java는 객체지향 언어이다.")
+
+
+class test_language_C(test_language): # test_language의 내용을 상속받아 사용 (super 사용가능)
+    def __init__(self, count, language):
+        test_language.__init__(self,count) # 클래스를 지정하여 사용할 경우 self 사용
+        language_characteristic.__init__(self,language) # 상속을 정의하지 않은 클래스도 사용가능
+    def count_print(self):
+        super().count_print() # 상속을 사용할 경우 self를 사용하지 않음
+    def language_print(self):
+        language_characteristic.characteristic_print(self)
+
+test_class_variable=test_language_C(count,language[count-1])
+test_class_variable.count_print()
+test_class_variable.language_print()
+count+=1
+
+class test_language_Java(test_language,language_characteristic): # 다중상속
+    def __init__(self, count, language):
+        test_language.__init__(self,count) 
+        language_characteristic.__init__(self,language) 
+    def count_print(self):
+        super().count_print() # 다중상속일 경우 가장 앞에 있는 클래스를 사용한다.
+    def language_print(self):
+        language_characteristic.characteristic_print(self)
+
+test_class_variable=test_language_Java(count,language[count-1])
+test_class_variable.count_print()
+test_class_variable.language_print()
+count+=1
