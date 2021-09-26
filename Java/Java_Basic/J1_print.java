978764306e025e53c12b3d8db159fe5f14a1fc1a
kevincms <kevincms314@naver.com>
2021-09-26 23:12:09 +0900

    2021-09-26 Python Basic complete Python Crawling&Scraping Start Day 1 Java Basic Start Day 1

---
+++ Java/Java_Basic/J1_print.java
@@ -0,0 +1,13 @@
+package Java_Basic;
+
+public class J1_print {
+    public static void main(String[] args) {
+        System.out.print("Hello Java");
+    }
+    // vscode 자동완성 main
+    // public static void main(String[] args) {
+    // }
+
+    // vscode 자동완성 sysout
+    // System.out.println();
+}
