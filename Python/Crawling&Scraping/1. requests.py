0bfb6dd3c0118aba3b329951fe56782efcbd36f5
kevincms314 <kevincms314@naver.com>
2021-10-29 22:17:34 +0900

    2021-10-29 Python Crawling&Scraping Proceed Day 2 백신 2차로 이틀 쉼.

--- Python/Crawling&Scraping/1. requests.py
+++ Python/Crawling&Scraping/1. requests.py
@@ -1,17 +1,24 @@
+import os
 import requests
-import requests
-from requests.api import request
+foler_path=os.path.dirname(__file__)
+file_name="\\test_google.html"
+create_file=foler_path+file_name
 url="https://www.google.co.kr/"
 count=1
 
 response=requests.get(url)
-response.raise_for_status()
+response.raise_for_status() # 2. 응답여부 확인 (if 문과 달리 오류를 발생시킴)
 
-print("%d. %d"%(count,response.status_code)) # 1. 응답코드
+# 1. 응답코드 200-정상
+print("%d. %d"%(count,response.status_code)) 
 count+=1
 
-print("%d. %d"%(count,requests.codes.ok))
+# 2. 응답여부 확인
+if response.status_code==requests.codes.ok:
+    print("{} 접근가능".format(count))
+else:
+    print("{} 접근불가능 [에러코드 {}]".format(count,response.status_code))
 count+=1
 
-print("%d. %s"%(count,response.json))
-count+=1
+with open(create_file,"w",encoding="utf8") as google:
+    google.write(response.text)
--- Python/Python_basic/1.2 file_input&output.py
+++ Python/Python_basic/1.2 file_input&output.py
@@ -1,34 +1,34 @@
 import os
 foler_path=os.path.dirname(__file__)
 file_name="\\test_file.txt"
+create_file=foler_path+file_name
 language=["Python","C","Java","C++","Go","JavaScript","Swift","C#"]
 count=1
 
-print("%d. %s"%(count,foler_path+file_name)) # 1. 파일경로+파일이름
-count+=1
+print("%d. %s"%(count,create_file)) # 1. 파일경로+파일이름
 
-test_file=open(foler_path+file_name,"w",encoding="utf8")
-print("[%d. %s]"%(count,language[count-1]),file=test_file) # w :  덮어쓰기 
+test_file=open(create_file,"w",encoding="utf8")
+print("[%d. %s]"%(count,language[count-1]),file=test_file) # w :  덮어쓰기
 count+=1
 test_file.close()
 
-test_file=open(foler_path+file_name,"a",encoding="utf8")
+test_file=open(create_file,"a",encoding="utf8")
 test_file.write("[%d. %s]"%(count,language[count-1])) # a : 이어쓰기
 count+=1
 test_file.close()
 
 count=2
-test_file=open(foler_path+file_name,"r",encoding="utf8")
-print("%d. %s"%(count,test_file.read())) # 모두 읽어오기
+test_file=open(create_file,"r",encoding="utf8")
+print("%d.\n%s"%(count,test_file.read())) # 모두 읽어오기
 count+=1
 test_file.close()
 
-test_file=open(foler_path+file_name,"r",encoding="utf8")
-print(test_file.readline()) # 한줄씩 읽어오기
+test_file=open(create_file,"r",encoding="utf8")
+print("%d. %s"%(count,test_file.readline()),end="") # 한줄씩 읽어오기
 count+=1
 test_file.close()
 
 # with은 자원을 획득 후 바로 반납할 때 사용한다. (with EXPRESSION [as VARIAVLE])
-with open(foler_path+file_name,"r",encoding="utf8") as test_file: # with을 사용하면 close를 하지 않아도 됨
-    print(test_file.readlines()) # 리스트로 모두 읽어오기
+with open(create_file,"r",encoding="utf8") as test_file: # with을 사용하면 close를 하지 않아도 됨
+    print("%d. %s"%(count,test_file.readlines())) # 리스트로 모두 읽어오기
 
