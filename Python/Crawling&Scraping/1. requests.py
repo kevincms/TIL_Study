0bb02df4a2010fff2512d8f27ae9ef2c89bbd0b1
kevincms <kevincms314@naver.com>
2021-11-01 21:15:07 +0900

    2021-10-30 Python Crawling&Scraping Proceed Day 3

--- Python/Crawling&Scraping/1. requests.py
+++ Python/Crawling&Scraping/1. requests.py
@@ -1,6 +1,5 @@
 import os
 import requests
-
 foler_path=os.path.dirname(__file__)
 file_name="\\test_google.html"
 create_file=foler_path+file_name
@@ -16,9 +15,9 @@ count+=1
 
 # 2. 응답여부 확인
 if response.status_code==requests.codes.ok:
-    print("{}. 접근가능".format(count))
+    print("{} 접근가능".format(count))
 else:
-    print("{}. 접근불가능 [에러코드 {}]".format(count,response.status_code))
+    print("{} 접근불가능 [에러코드 {}]".format(count,response.status_code))
 count+=1
 
 with open(create_file,"w",encoding="utf8") as google:
--- record.md
+++
@@ -1,9 +0,0 @@
-() Start Day 1
-() Proceed Day 2~
-() Complete
-() Modify & Add
-
-Jupyter Notebook Basic Proceed Day 5
-Java Basic Proceed Day 3
-Python Crawling&Scraping Proceed Day 2
-Python Basic complete
