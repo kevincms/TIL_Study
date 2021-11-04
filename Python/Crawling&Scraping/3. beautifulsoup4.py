15a31b9d6ce76740c0a73969a0a453f0a7f54d3c
kevincms <kevincms314@naver.com>
2021-11-04 20:29:15 +0900

    2021-11-04 Python Crawling&Scraping Proceed Day 6

--- Python/Crawling&Scraping/3. beautifulsoup4.py
+++ Python/Crawling&Scraping/3. beautifulsoup4.py
@@ -19,20 +19,28 @@ count+=1
 print("%d. %s %s"%(count,soup.a["href"],soup.a.attrs["href"])) # 4. 특정 속성 추출
 count+=1
 
-webtoon_rank_1=soup.find("li",attrs={"class":"rank01"})
-print("%d. (rank1) %s"%(count,webtoon_rank_1.a["title"])) # 5. html 특정 태그 추출
+webtoon_rank_1=soup.find("li",attrs={"class":"rank01"}) # 태그의 속성 이용
+title=webtoon_rank_1.a["title"]
+rank1_text=soup.find("a",text=title).get_text() # 태그의 텍스트 이용
+print("%d. (rank1) %s %s"%(count,title,webtoon_rank_1.a["title"])) # 5. html 특정 태그 추출
+count+=1
+
+webtoon_title=soup.find_all("a",attrs={"class":"title"}) # 모든 태그를 리스트의 형태로 반환
+print("%d. "%count,end="")
+for title in webtoon_title[:3]:
+    print(title.get_text(),end=" ") # 6. html 특정 모든 태그 추출
 count+=1
 
 webtoon_rank=webtoon_rank_1.parent
-print("%d. %s"%(count,webtoon_rank["class"])) # 6. 부모 태그 추출
+print("%d. %s"%(count,webtoon_rank["class"])) # 7. 부모 태그 추출
 count+=1
 
 webtoon_rank_2=webtoon_rank_1.next_sibling.next_sibling # 설정에 따라 여러번 sibling을 해야 할 수 있음
 webtoon_rank_1=webtoon_rank_2.previous_sibling.previous_sibling
-print("%d. (rank1) %s (rank2) %s"%(count,webtoon_rank_1.a["title"],webtoon_rank_2.a["title"])) # 6. 다음 태그 추출
+print("%d. (rank1) %s (rank2) %s"%(count,webtoon_rank_1.a["title"],webtoon_rank_2.a["title"])) # 8. 다음 태그 추출
 count+=1
 
 webtoon_rank_2=webtoon_rank_1.find_next_sibling("li") # find_next_sibling 을 이용해 다음 태그를 설정할 수 있음
 webtoon_rank_1=webtoon_rank_2.find_previous_sibling("li")
-print("%d. (rank1) %s (rank2) %s"%(count,webtoon_rank_1.a["title"],webtoon_rank_2.a["title"])) # 6. 다음 태그 추출
+print("%d. (rank1) %s (rank2) %s"%(count,webtoon_rank_1.a["title"],webtoon_rank_2.a["title"])) # 9. 다음 태그 추출
 count+=1
