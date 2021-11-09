10ae4ae579ccd565eef45afb888b72e0b11d09f5
kevincms314 <kevincms314@naver.com>
2021-11-09 21:12:12 +0900

    2021-11-09 Python Crawling&Scraping Proceed Day 11

--- Python/Project/baekjoon/test.py
+++ Python/Project/baekjoon/test.py
@@ -10,7 +10,7 @@ response=requests.get(url)
 response.raise_for_status()
 soup=bs4.BeautifulSoup(response.text,"lxml")
 
-file_name="/test.txt"
+file_name="\\test.html"
 create_file=folder_path+file_name
 
 tr_list=soup.find("tbody").find_all("tr")
@@ -28,7 +28,16 @@ soup=bs4.BeautifulSoup(response.text,"lxml")
 problem_tr_list=soup.find("tbody").find_all("tr")
 # print(problem_tr_list) # 리스트가 문제 수 의 2배로 되어 있음. 문제사이 텍스트가 추가되어 있음.
 
+problem_num=problem_tr_list[0].find("td").get_text()
+problem_title=problem_tr_list[0].find("td").find_next_sibling("td").find_next_sibling("td").get_text()
+problem_file_name=problem_num+". "+problem_title
+print(problem_file_name)
 
+down_url=baekjoon_url+problem_tr_list[0].find("a")["href"]
+response=requests.get(down_url)
+response.raise_for_status()
+# with open(create_file,"w",encoding="utf-8") as down_html:
+#     down_html.write(response.text)
 
 # # for i in range(12):
     
