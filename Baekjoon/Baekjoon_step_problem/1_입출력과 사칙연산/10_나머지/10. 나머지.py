b1c8d95527ba5cd4f3ef73ef681ef9cbd9d0a8dc
kevincms <kevincms314@naver.com>
2021-11-16 19:05:40 +0900

    2021-11-16 Baekjoon Proceed Day 4

--- Baekjoon/Baekjoon_step_problem/1_입출력과 사칙연산/10_나머지/10. 나머지.py
+++ Baekjoon/Baekjoon_step_problem/1_입출력과 사칙연산/10_나머지/10. 나머지.py
@@ -6,12 +6,23 @@
 출력
  -첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
 예제 입력 1
- -5 8 4
-
+ -5 8 4
 예제 출력 1
- -1
-1
-0
-0
-
+ -
+1
+1
+0
+0
 '''
+
+input_num_list=input().split()
+num=[]
+for i in input_num_list:
+    num.append(int(i))
+a=num[0]
+b=num[1]
+c=num[2]
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
--- record.md
+++ record.md
@@ -6,6 +6,6 @@
 Jupyter Notebook Basic Proceed Day 5
 Java Basic Proceed Day 2
 Python Crawling&Scraping Proceed Day 15
-Baekjoon Proceed Day 3
+Baekjoon Proceed Day 4
 CLI Start Day 1
 Python Basic Modify
