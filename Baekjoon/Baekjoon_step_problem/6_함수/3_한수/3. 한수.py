0ad8be349936228ba4486fd154375c746a769658
kevincms314 <kevincms314@naver.com>
2021-12-22 18:35:57 +0900

    2021-12-22 Baekjoon Proceed Day 28

--- Baekjoon/Baekjoon_step_problem/6_함수/3_한수/3. 한수.py
+++ Baekjoon/Baekjoon_step_problem/6_함수/3_한수/3. 한수.py
@@ -7,33 +7,44 @@
  -첫째 줄에 1보다 크거나 같고, N보다 작거나 같은 한수의 개수를 출력한다.
 예제 입력 1
  -110
-
 예제 출력 1
  -99
-
 예제 입력 2
  -1
-
 예제 출력 2
  -1
-
 예제 입력 3
  -210
-
 예제 출력 3
  -105
-
 예제 입력 4
  -1000
-
 예제 출력 4
  -144
-
 예제 입력 5
  -500
-
 예제 출력 5
  -119
-
 https://www.acmicpc.net//problem/1065
 '''
+def digit_list(input_num):
+    ouput_list=[]
+    while 1:
+        if input_num==0: break
+        ouput_list.append(input_num%10)
+        input_num=int(input_num/10)
+    return ouput_list
+
+input_num=int(input())
+count=0
+for i in range(input_num):
+    series_list=digit_list(i+1)
+    digit_size=len(series_list)
+    if digit_size==1: count+=1
+    else:
+        diff_num=series_list[0]-series_list[1]
+        for j in range(digit_size-1):
+            compare_diff=series_list[j]-series_list[j+1]
+            if compare_diff!=diff_num: break
+            elif j==digit_size-2: count+=1
+print(count)
