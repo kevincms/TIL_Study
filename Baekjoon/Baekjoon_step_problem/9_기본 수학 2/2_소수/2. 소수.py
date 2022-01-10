2b722b8b972a9b525395078078ac886cc70b6320
kevincms314 <kevincms314@naver.com>
2022-01-10 09:51:43 +0900

    2022-01-10 Baekjoon Proceed Day 46

--- Baekjoon/Baekjoon_step_problem/9_기본 수학 2/2_소수/2. 소수.py
+++ Baekjoon/Baekjoon_step_problem/9_기본 수학 2/2_소수/2. 소수.py
@@ -6,22 +6,64 @@
  -입력의 첫째 줄에 M이, 둘째 줄에 N이 주어진다.
 M과 N은 10,000이하의 자연수이며, M은 N보다 작거나 같다.
 출력
- -M이상 N이하의 자연수 중 소수인 것을 모두 찾아 첫째 줄에 그 합을, 둘째 줄에 그 중 최솟값을 출력한다. 
+ -M이상 N이하의 자연수 중 소수인 것을 모두 찾아 첫째 줄에 그 합을, 둘째 줄에 그 중 최솟값을 출력한다. 
 단, M이상 N이하의 자연수 중 소수가 없을 경우는 첫째 줄에 -1을 출력한다.
 예제 입력 1
- -60
-100
-
+ -60
+100
 예제 출력 1
- -620
-61
-
+ -620
+61
 예제 입력 2
- -64
-65
-
+ -64
+65
 예제 출력 2
- --1
-
+ --1
 https://www.acmicpc.net//problem/2581
+'''
+
+import sys
+input=sys.stdin.readline
+start_num=int(input())
+end_num=int(input())
+prime_list=[True for i in range(end_num+1)]
+prime_list[:2]=[False,False]
+for i in range(end_num+1):
+    if prime_list[i]==True:
+        for j in range(2*i,end_num+1,i):
+            prime_list[j]=False
+prime_snm=0
+for i in range(end_num,start_num-1,-1):
+    if prime_list[i]==True:
+        prime_snm+=i
+        least_prime=i
+if prime_snm>0:
+    print(prime_snm)
+    print(least_prime)
+else: print(-1)
+
+'''
+import sys
+input=sys.stdin.readline
+start_num=int(input())
+end_num=int(input())
+prime_list=[i+2 for i in range(end_num-1)]
+for i in prime_list:
+    multiply=1
+    while 1:
+        multiply+=1
+        if i*multiply>end_num: break
+        try: prime_list.remove(i*multiply)
+        except ValueError: continue
+for i in range(start_num,end_num+1):
+    try:
+        start_prime_index=prime_list.index(i)
+        break
+    except ValueError:
+        if i==end_num:
+            print(-1)
+            exit()
+        continue
+print(sum(prime_list[start_prime_index:]))
+print(prime_list[start_prime_index])
 '''
