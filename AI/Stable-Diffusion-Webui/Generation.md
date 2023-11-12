c90a00568cc3a8287f5796ce56b6c72b1c550682
kevincms <kevincms314@naver.com>
2023-11-12 22:31:29 +0900

    한글 형식 깨짐 + Stable-Diffusion-Webui 메인화면 간단 정리

---
+++ AI/Stable-Diffusion-Webui/Generation.md
@@ -0,0 +1,11 @@
+# Stable-Diffusion-Webui 기본설정
+<img src="https://raw.githubusercontent.com/kevincms/image/main/stable%20diffusion/stable%20diffusion%20%EB%A9%94%EC%9D%B8%ED%99%94%EB%A9%B4.png" width="70%" height="70%"/>
+
+> 1. model 선택[<span style="color:red">**1**</span>]  
+> 2. 이미지 생성 방식 및 extension 추가 설치[<span style="color:red">**2**</span>]  
+> 3. Generation Prompt[<span style="color:red">**3**</span>]
+>> - 상단 Prompt는 목표하는 것을 하단 Prompt는 제외하고 싶은 것을 입력
+> 4. Sampling method, steps [<span style="color:red">**4**</span>]  
+>> - method 방식은 큰 차이가 없는 것 같음.
+>> - steps 가 높을수록 더 선명해짐7
+> 5. 사진 크기 조정 [<span style="color:red">**4**</span>]  
--- C & C++/C++_basic/3_data_type_variable.cpp
+++ C & C++/C++_basic/3_data_type_variable.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main(){
     int count=1, test_int=10;
-    printf("%d. %d | size: %d\n",count++,test_int,sizeof(test_int)); // 1. Á¤¼öÇü º¯¼ö
+    printf("%d. %d | size: %d\n",count++,test_int,sizeof(test_int)); // 1. ì •ìˆ˜í˜• ë³€ìˆ˜
     
     // long = long int, long long = long long int 
     long test_long=10;
-    printf("%d. %d | size: %d\n",count++,test_long,sizeof(test_long)); // 2. Á¤¼öÇü º¯¼ö
+    printf("%d. %d | size: %d\n",count++,test_long,sizeof(test_long)); // 2. ì •ìˆ˜í˜• ë³€ìˆ˜
 
     long long test_longlong=10;
-    printf("%d. %d | size: %d\n",count++,test_longlong,sizeof(test_longlong)); // 2. Á¤¼öÇü º¯¼ö
+    printf("%d. %d | size: %d\n",count++,test_longlong,sizeof(test_longlong)); // 2. ì •ìˆ˜í˜• ë³€ìˆ˜
 }
