5f2267e4fbfadfd889c5e4ca83edb39006589106
kevincms <kevincms314@naver.com>
2023-12-01 22:35:42 +0900

    8번 백준 실패. 원인 못 찾음

--- DO IT/Python/2. 투 포인터/008.py
+++ DO IT/Python/2. 투 포인터/008.py
@@ -1,14 +1,19 @@
+# https://www.acmicpc.net/problem/1253
 import sys
 
-def two_point_search(input_num,input_list):
+def two_point_search(input_num,input_list,self_index):
     start_index=0
     end_index=len(input_list)-1
-    while(start_index!=input_num-1):
-        if start_index==end_index: return False
+    while(start_index<end_index):
         temp_sum=input_list[start_index]+input_list[end_index]
-        if temp_sum==input_num: return True
+        if temp_sum==input_num:
+            if start_index==self_index: start_index=start_index+1
+            elif end_index==self_index: end_index=end_index-1
+            else: return True
         elif temp_sum>input_num: end_index=end_index-1
         else: start_index=start_index+1
+    return False
+        
 
 input=sys.stdin.readline
 input_count=int(input())
@@ -17,6 +22,6 @@ output_num=0
 
 for i in range(input_count):
     temp_num=input_list[i]
-    is_good_num=two_point_search(temp_num,input_list)
+    is_good_num=two_point_search(temp_num,input_list,i)
     if is_good_num: output_num=output_num+1
 print(output_num)
