630af91a8356b09df6bef6aade2ed141b693685c
kevincms <kevincms314@naver.com>
2023-12-07 22:58:58 +0900

    Node js Basic

--- Nodejs/Nodejs_Basic/1. print.js
+++ Nodejs/Nodejs_Basic/1. print.js
@@ -1,1 +1,10 @@
-console.log("test")
+// Nodejs의 경우 console.log로 출력할 수 있다.
+language=["Python","C","Nodejs","C++","Go","JavaScript","Swift","C#"]
+count=1
+
+// +로 문자열과 변수를 섞어 출력할 수 있다.
+console.log(count+". "+language[count++-1])
+
+// ,로 문자열과 변수를 섞어 출력할 수 있다. (단 +와 달리 자동으로 한칸 공백이 생긴다.)
+temp_count=String(count)+"."
+console.log(temp_count,language[count-1])
