fc95066df1525c0d6ea2424a0a16641fc0c57642
kevincms <kevincms314@naver.com>
2023-12-09 22:25:30 +0900

    node js print 추가

---
+++ CLI/Nodejs/Nodejs.md
@@ -0,0 +1,10 @@
+# Nodejs & npm
+
+### Nodejs 버전 확인
+- Node -v
+
+### npm 버전 확인
+- npm -v
+
+### npm 프로젝트 생성
+- npm init
--- Nodejs/Nodejs_Basic/1. print.js
+++ Nodejs/Nodejs_Basic/1. print.js
@@ -7,4 +7,7 @@ console.log(count+". "+language[count++-1])
 
 // ,로 문자열과 변수를 섞어 출력할 수 있다. (단 +와 달리 자동으로 한칸 공백이 생긴다.)
 temp_count=String(count)+"."
-console.log(temp_count,language[count-1])
+console.log(temp_count,language[count++-1])
+
+// `를 이용해 문자열안에 변수를 ${}로 넣을 수 있다. (`=키보드 ~ 위치)
+console.log(`${count}. ${language[count++-1]}`,)
