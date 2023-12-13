1fa34599af93fdb05ac01db76907452ad227ff9b
kevincms <kevincms314@naver.com>
2023-12-13 22:52:18 +0900

    10번 풀다가 잘못됨을 느낌

--- DO IT/Python/3. 슬라이딩 윈도우/010.py
+++ DO IT/Python/3. 슬라이딩 윈도우/010.py
@@ -10,20 +10,24 @@ input=sys.stdin.readline
 num_count, slide_length=map(int,input().split())
 num_list=list(map(int,input().split()))
 
-slide_min_num=1000000001
-slide_min_num_2=1000000001
+slide_min_num=[num_list[0] for _ in range(slide_length)]
 output_list=[0 for _ in range(num_count)]
 
-for i in range(slide_length):
-    if slide_min_num>=num_list[i]: slide_min_num=num_list[i]
-    else:
-        if slide_min_num_2>num_list[i]: slide_min_num_2=num_list[i]
-output_list[0]=slide_min_num
+def min_num_sort(num_list,num):
+    if num_list==[]: return []
+    if num_list[0]>num:
+        num_list[0]=num
+        return num_list
+    else: return num_list[:1]+min_num_sort(num_list[1:],num)
 
-for i in range(num_count-slide_length-1):
-    add_index=i+slide_length
-    sub_index=i+1
-    slide_list=num_list[sub_index:add_index+1]
+for i in range(slide_length):
+    slide_min_num=min_num_sort(slide_min_num,num_list[i])
+    output_list[i]=slide_min_num[0]
+
+for i in range(num_count-slide_length):
+    end_index=i+slide_length
+    start_index=i+1
+    slide_list=num_list[start_index:end_index+1]
 
     if not (slide_min_num in slide_list):
         slide_min_num=slide_min_num_2
