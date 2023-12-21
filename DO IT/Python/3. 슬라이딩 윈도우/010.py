8e5ef251ef7336abf717cfbc3c02917bd0191796
kevincms <kevincms314@naver.com>
2023-12-21 22:28:45 +0900

    슬라이딩 윈도우 10 + deque 시간 초과 실패

--- DO IT/Python/3. 슬라이딩 윈도우/010.py
+++ DO IT/Python/3. 슬라이딩 윈도우/010.py
@@ -1,42 +1,34 @@
 # https://www.acmicpc.net/problem/11003
 
-# 12 3
-# 1 5 2 3 6 2 3 7 3 5 2 6
-# 1 1 1 2 2 2 2 2 3 3 2 2
-
 import sys
+from collections import deque
 
 input=sys.stdin.readline
 num_count, slide_length=map(int,input().split())
 num_list=list(map(int,input().split()))
 
-slide_min_num=[num_list[0] for _ in range(slide_length)]
+slide_deque=deque([num_list[0]])
 output_list=[0 for _ in range(num_count)]
+output_list[0]=num_list[0]
 
-def min_num_sort(num_list,num):
-    if num_list==[]: return []
-    if num_list[0]>num:
-        num_list[0]=num
-        return num_list
-    else: return num_list[:1]+min_num_sort(num_list[1:],num)
-
-for i in range(slide_length):
-    slide_min_num=min_num_sort(slide_min_num,num_list[i])
-    output_list[i]=slide_min_num[0]
+for i in range(slide_length-1):
+    comp_num=num_list[i+1]
+    if slide_deque[0]>comp_num: slide_deque.appendleft(comp_num)
+    else:
+        while(slide_deque[-1]>comp_num): slide_deque.pop()
+        slide_deque.append(comp_num)
+    output_list[i+1]=slide_deque[0]
 
 for i in range(num_count-slide_length):
-    end_index=i+slide_length
-    start_index=i+1
-    slide_list=num_list[start_index:end_index+1]
-
-    if not (slide_min_num in slide_list):
-        slide_min_num=slide_min_num_2
-        slide_min_num_2=1000000001
-    elif not (slide_min_num_2 in slide_list): slide_min_num_2=1000000001
-    for j in range(slide_length):
-        if slide_min_num>=slide_list[j]: slide_min_num=slide_list[j]
-        else:
-            if slide_min_num_2>slide_list[j]: slide_min_num_2=slide_list[j]
-    output_list[i+1]=slide_min_num
+    add_index=i+slide_length
+    sub_index=i
+    add_num=num_list[add_index]
+    sub_num=num_list[sub_index]
+    try: slide_deque.remove(sub_num)
+    except: pass
+    if slide_deque[0]>add_num: slide_deque.appendleft(add_num)
+    else:
+        while(slide_deque[-1]>add_num): slide_deque.pop()
+        slide_deque.append(add_num)
+    output_list[add_index]=slide_deque[0]
 print(*output_list)
-    
