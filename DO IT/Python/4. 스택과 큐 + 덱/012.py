822c041ad2e728b879e5fc8f36eb820b6d8845e6
kevincms <kevincms314@naver.com>
2023-12-30 21:46:58 +0900

    12 stack 해결 조건 하나 빼먹음

--- DO IT/Python/4. 스택과 큐 + 덱/012.py
+++ DO IT/Python/4. 스택과 큐 + 덱/012.py
@@ -16,16 +16,17 @@ for i in range(input_count):
     if len(bigger_stack)==0: ouput_list[output_index]=-1
     elif num_stack[-1]<bigger_stack[-1]: ouput_list[output_index]=bigger_stack[-1]
     else:
-        while(num_stack[-1]>bigger_stack[-1]):
+        while(num_stack[-1]>=bigger_stack[-1]):
             bigger_stack.pop()
-            if len(bigger_stack)==0: break
-        if len(bigger_stack)==0: ouput_list[output_index]=-1
-        else: ouput_list[output_index]=bigger_stack[-1]
+            if len(bigger_stack)==0:
+                ouput_list[output_index]=-1
+                break
+        if len(bigger_stack)!=0: ouput_list[output_index]=bigger_stack[-1]
 
     temp=num_stack.pop()
     if len(bigger_stack)==0: bigger_stack.append(temp)
     else:
-        while(temp>bigger_stack[-1]):
+        while(temp>=bigger_stack[-1]):
             bigger_stack.pop()
             if len(bigger_stack)==0: break
         bigger_stack.append(temp)
