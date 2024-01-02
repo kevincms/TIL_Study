43014b53ee23cd72d9114fd67c13b3c8189cca0e
kevincms <kevincms314@naver.com>
2024-01-02 22:46:38 +0900

    14 우선순위 큐 모듈 사용하여 해결

--- DO IT/Python/4. 스택과 큐 + 덱/014.py
+++ DO IT/Python/4. 스택과 큐 + 덱/014.py
@@ -1,19 +1,16 @@
 # https://www.acmicpc.net/problem/11286
 
 import sys
+from queue import PriorityQueue
 
 input=sys.stdin.readline
 cal_num=int(input())
-heap_list=[]
+heap_queue=PriorityQueue()
 before_sort_check=False
 
 for i in range(cal_num):
     input_num=int(input())
     if input_num==0:
-        if heap_list:
-            if before_sort_check: print(heap_list.pop()[1])
-            else: 
-                heap_list.sort(reverse=True)
-                print(heap_list.pop()[1])
-        else: print("0")
-    else: heap_list.append([abs(input_num),input_num])
+        if heap_queue.empty(): print("0")
+        else: print(heap_queue.get()[1])
+    else: heap_queue.put([abs(input_num),input_num])
