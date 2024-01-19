6f8f7a966984d0176399a398af332e20f05034e7
kevincms <kevincms314@naver.com>
2024-01-19 22:27:09 +0900

    19번 퀵 정렬 쉽지 않다. 좀 더 걸릴 듯

--- DO IT/Python/5. 정렬/019.py
+++ DO IT/Python/5. 정렬/019.py
@@ -1,28 +1,14 @@
 # https://www.acmicpc.net/problem/11004
-
-
-# def quick_sort(input_list):
-#     num_count=len(input_list)
-#     if num_count<=1: return input_list
-#     else: 
-#         pivot_num=input_list[0]
-#         pivot_count=1
-#         front_list=[]
-#         back_list=[]
-#         for i in range(num_count-1):
-#             comp_num=input_list[i+1]
-#             if pivot_num>comp_num: front_list.append(comp_num)
-#             elif pivot_num<comp_num: back_list.append(comp_num)
-#             else: pivot_count=pivot_count+1
-#         return quick_sort(front_list)+[pivot_num]*pivot_count+quick_sort(back_list)
     
-def pivot_divide(input_list,pivot_num):
+
+def pivot_divide(input_list):
+    pivot_num=input_list[0]
     num_count=len(input_list)
     front_list=[]
     back_list=[]
     pivot_count=1
-    for i in range(num_count):
-        comp_num=input_list[i]
+    for i in range(num_count-1):
+        comp_num=input_list[i+1]
         if pivot_num>comp_num: front_list.append(comp_num)
         elif pivot_num<comp_num: back_list.append(comp_num)
         else: pivot_count=pivot_count+1
@@ -36,8 +22,7 @@ front_count=0
 back_count=0
 sort_list=num_list
 while(1):
-    pivot_num=sort_list[0]
-    front_list, back_list, pivot_count=pivot_divide(sort_list,pivot_num)
+    front_list, back_list, pivot_count=pivot_divide(sort_list)
     front_list_size=len(front_list)
     back_list_size=len(back_list)
 
@@ -46,6 +31,7 @@ while(1):
 
     front_count=front_count+front_list_size
     back_count=back_count+back_list_size
+    front_pivot_sum=front_count+pivot_count
     if front_count>=num_order:
         if front_list_size==1 and num_order==front_count:
             output_num=front_list[0]
@@ -53,15 +39,15 @@ while(1):
         front_count=before_front_count
         back_count=back_count+pivot_count
         sort_list=front_list
-    elif front_count+pivot_count>=num_order:
+    elif front_pivot_sum>=num_order:
         output_num=pivot_count
         break
     else:
-        if front_list_size==1 and num_order==back_count:
+        if back_list_size==1 and num_order==front_pivot_sum+1:
             output_num=back_list[0]
             break
         back_count=before_back_count
         front_count=front_count+pivot_count
-        sort_list=back_count
+        sort_list=back_list
 
 print(output_num)
