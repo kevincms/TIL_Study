ba93ef7d669ed2cb9b8bfbcdd4fcf2aa01b63b56
kevincms <kevincms314@naver.com>
2024-02-01 22:42:14 +0900

    시간초과 구상 방안1 실패

--- DO IT/Python/5. 정렬/021.py
+++ DO IT/Python/5. 정렬/021.py
@@ -1,11 +1,22 @@
 # https://www.acmicpc.net/problem/1517
 
+from collections import deque
+
 num_count=int(input())
-num_list=list(map(int,input().split()))
+num_queue=deque(map(int,input().split()))
+count_stack=deque([])
 
 output_count=0
-for i in range(num_count-1):
-    for j in range(num_count-1-i):
-        if num_list[i]>num_list[j+i+1]: output_count=output_count+1
-
+for i in range(num_count):
+    if len(count_stack)==0:
+        count_stack.append(num_queue[0])
+        num_queue.popleft()
+    else:
+        while(count_stack[-1]<num_queue[0]):
+            count_stack.pop()
+            if len(count_stack)==0: break
+        output_count=output_count+len(count_stack)
+        count_stack.append(num_queue[0])
+        num_queue.popleft()
+        
 print(output_count)
