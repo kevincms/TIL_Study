cfef330cd5491b1649db45ea74cc20d76ed436c1
kevincms <kevincms314@naver.com>
2024-02-07 22:38:29 +0900

    기수 정렬 성공

--- DO IT/Python/5. 정렬/022.py
+++ DO IT/Python/5. 정렬/022.py
@@ -3,12 +3,13 @@ import sys
 
 input=sys.stdin.readline
 input_count=int(input())
-num_list=[0 for _ in range(input_count+1)]
+max_num_value=10000
+num_list=[0 for _ in range(max_num_value+1)]
 
-for i in range(input_count):
+for _ in range(input_count):
     input_num=int(input())
-    num_list[input_num]=num_list[input_num]+1
-for i in range(input_count):
+    num_list[input_num]+=1
+for i in range(max_num_value):
     num=i+1
     temp_num=num_list[num]
     if temp_num: 
