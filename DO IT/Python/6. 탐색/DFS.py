92cbc94548a7fa063fb1eebd2db0f92b0c791641
kevincms <kevincms314@naver.com>
2024-02-11 22:24:39 +0900

    DFS 필요없는 코드 삭제

--- DO IT/Python/6. 탐색/DFS.py
+++ DO IT/Python/6. 탐색/DFS.py
@@ -12,17 +12,15 @@ graph_list[2].append(4)
 
 graph_list[3].append(6)
 
-check_list=[False for i in range(6)]
+check_list=[False for _ in range(6)]
 
 def DFS(graph_list, check_list):
     graph_size=len(graph_list)
     compelte_list=[]
     stack=deque()
-    start_index=0
 
-    temp_list=graph_list[start_index]
-    stack.append(temp_list[0])
-    check_list[temp_list[0]-1]=True
+    stack.append(graph_list[0][0])
+    check_list[graph_list[0][0]-1]=True
 
     while(1):
         if len(stack)==0: return compelte_list, check_list
@@ -39,10 +37,6 @@ def DFS(graph_list, check_list):
                 if check_list[check_node-1]: continue
                 stack.append(check_node)
                 check_list[temp_list[i]-1]=True
-        while(1):
-            if start_index==graph_size-1: break
-            if not(check_list[start_index]): break
-            start_index+=1
 
 
 print(DFS(graph_list, check_list))
