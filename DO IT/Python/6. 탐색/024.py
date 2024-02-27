e0c7f5c17d94ab0927dbf7dff435a9cf0ad26422
“kevincms” <kevincms314@naver.com>
2024-02-27 10:39:16 +0900

    24번 메모리 문제 해결시도 실패

--- DO IT/Python/6. 탐색/024.py
+++ DO IT/Python/6. 탐색/024.py
@@ -1,22 +1,24 @@
 # https://www.acmicpc.net/problem/2023
 
+def is_prime(num):
+    if num in prime_list: return True
+    for i in prime_list:
+        if num%i==0: return False
+    return True
+
 def DFS(digit):
-    if digit==1:
-        output_list=[]
-        for i in range(10):
-            if prime_list[i]: output_list.append(i)
-        return output_list
+    if digit==1: return [2,3,5,7]
     else:
         temp_list=DFS(digit-1)
         output_list=[]
         for i in temp_list:
             for j in range(10):
                 num=i*10+j
-                if prime_list[num]: output_list.append(num)
+                if is_prime(num): output_list.append(num)
         return output_list
 
 num_digit=int(input())
-max_num=10**num_digit+1
+max_num=1000000
 prime_list=[True for _ in range(max_num)]
 
 prime_list[0]=False
@@ -24,5 +26,12 @@ prime_list[1]=False
 for i in range(2,max_num):
     if prime_list[i]:
         for j in range(2*i,max_num,i): prime_list[j]=False
+
+temp_list=[]
+for i in range(max_num):
+    if prime_list[i]: temp_list.append(i)
+
+prime_list.clear()
+temp_list=prime_list
 
 print(*DFS(num_digit),sep="\n")
