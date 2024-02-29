e09a71e4c6689a458b1cb41d65cf6d322eef1315
“kevincms” <kevincms314@naver.com>
2024-02-29 09:43:47 +0900

    DFS 재귀 이용한 방법으로 수정

--- DO IT/Python/6. 탐색/DFS.py
+++ DO IT/Python/6. 탐색/DFS.py
@@ -1,6 +1,7 @@
 from collections import deque
 
-graph_list=[[i+1] for i in range(6)]
+node_num=6
+graph_list=[[i+1] for i in range(node_num)]
 
 graph_list[0].append(2)
 graph_list[0].append(3)
@@ -14,29 +15,22 @@ graph_list[3].append(6)
 
 check_list=[False for _ in range(6)]
 
-def DFS(graph_list, check_list):
-    graph_size=len(graph_list)
-    compelte_list=[]
-    stack=deque()
+def DFS(stack=deque([0]),output_list=[],check_list=check_list):
+    if len(stack)==0: return output_list
+    node=stack[-1]
+    stack.pop()
+    output_list.append(node+1)
+    check_list[node]=True
 
-    stack.append(graph_list[0][0])
-    check_list[graph_list[0][0]-1]=True
+    arc_list=graph_list[node]
+    arc_len=len(arc_list)
+    if arc_len==1: return DFS(stack,output_list)
+    else:
+        for i in range(1,arc_len):
+            node_index=arc_list[i]-1
+            if not check_list[node_index]: stack.append(node_index)
+        return DFS(stack,output_list)
 
-    while(1):
-        if len(stack)==0: return compelte_list, check_list
-
-        previous_top=stack[-1]
-        temp_list=graph_list[previous_top-1]
-        compelte_list.append(previous_top)
-        stack.pop()
-
-        temp_len=len(temp_list)
-        if temp_len!=1:
-            for i in range(1,temp_len):
-                check_node=temp_list[i]
-                if check_list[check_node-1]: continue
-                stack.append(check_node)
-                check_list[temp_list[i]-1]=True
-
-
-print(DFS(graph_list, check_list))
+start_index=0
+stack=deque([start_index])
+print(DFS())
