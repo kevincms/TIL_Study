523801f7f7d73c8bbdbae6f92e646f48f2ec7a56
“kevincms” <kevincms314@naver.com>
2024-03-01 11:29:13 +0900

    025 DFS 리스트 인수 전달 시 문제 발견

--- DO IT/Python/6. 탐색/025.py
+++ DO IT/Python/6. 탐색/025.py
@@ -3,38 +3,28 @@
 import sys
 from collections import deque
 
-def DFS(graph_list, start_index):
-    graph_size=len(graph_list)
-    check_list=[False for _ in range(graph_size)]
-    compelte_list=[]
-    stack=deque()
-
-    stack.append(graph_list[start_index][0])
-    check_list[graph_list[start_index][0]-1]=True
-
-    while(1):
-        if len(stack)==0: return compelte_list
-
-        previous_top=stack[-1]
-        temp_list=graph_list[previous_top]
-        compelte_list.append(previous_top)
-        stack.pop()
-
-        temp_len=len(temp_list)
-        if temp_len!=1:
-            for i in range(1,temp_len):
-                check_node=temp_list[i]
-                if check_list[check_node-1]: continue
-                    # if temp_list[i] <= check_node: continue
-                    # if temp_list[i] in graph_list[check_node-1][1:]: continue
-                    # else: return compelte_list
-                stack.append(check_node)
-                check_list[temp_list[i]-1]=True
-                break
-
 input=sys.stdin.readline
 people_num, friend_num=map(int,input().split())
 graph_list=[[i] for i in range(people_num)]
+check_list=[False for _ in range(people_num)]
+
+def DFS(stack,output_list,check_list):
+    if len(stack)==0: return output_list
+    node=stack[-1]
+    stack.pop()
+    output_list.append(node)
+
+    arc_list=graph_list[node]
+    arc_len=len(arc_list)
+    if arc_len==1: return DFS(stack,output_list,check_list)
+    else:
+        for i in range(1,arc_len):
+            node_index=arc_list[i]
+            if not check_list[node_index]:
+                stack.append(node_index)
+                check_list[node_index]=True
+                break
+        return DFS(stack,output_list,check_list)
 
 for i in range(friend_num):
     num_1, num_2=map(int,input().split())
@@ -43,9 +33,40 @@ for i in range(friend_num):
 
 check_num=0
 for i in range(people_num):
-    output_list=DFS(graph_list,i)
-    if len(output_list)>=4:
+    stack=deque([i])
+    check_list[i]=True
+    temp_list=DFS(stack,[],check_list[:])
+    check_list[i]=False
+    # print(temp_list)
+    if len(temp_list)>=5:
         check_num=1
         break
-# print(output_list)
+# print(temp_list)
 print(check_num)
+
+"""
+6 5
+0 1
+0 2
+0 3
+0 4
+0 5
+
+5 4
+0 1
+1 2
+2 3
+3 4
+
+5 5
+0 1
+1 3
+1 4
+4 3
+3 2
+"""
+
+'''
+0 1 3 4
+2 3 4 1 0
+'''
