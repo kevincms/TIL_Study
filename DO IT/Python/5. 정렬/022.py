adb44702670724eec14735126c6fdaba96443b2a
“kevincms” <kevincms314@naver.com>
2024-03-04 02:43:22 +0900

    022 기수 정렬 메모리초과

--- DO IT/Python/5. 정렬/022.py
+++ DO IT/Python/5. 정렬/022.py
@@ -1,16 +1,26 @@
 # https://www.acmicpc.net/problem/10989
+
 import sys
+from collections import deque
 
 input=sys.stdin.readline
 input_count=int(input())
-max_num_value=10000
-num_list=[0 for _ in range(max_num_value+1)]
+max_num_=10000
+max_digit=5
+digit_queue_list=[deque([]) for _ in range(10)]
+num_queue=deque([])
 
-for _ in range(input_count):
-    input_num=int(input())
-    num_list[input_num]+=1
-for i in range(max_num_value):
-    num=i+1
-    temp_num=num_list[num]
-    if temp_num: 
-        for _ in range(temp_num): print(num)
+for i in range(input_count): num_queue.append(int(input()))
+for i in range(max_digit):
+    for j in range(input_count):
+        temp=num_queue[0]
+        digit_num=(temp//(10**i))%10
+        num_queue.popleft()
+        digit_queue_list[digit_num].append(temp)
+    for j in range(10):
+        temp_queue=digit_queue_list[j]
+        while(len(temp_queue)!=0):
+            temp=temp_queue[0]
+            temp_queue.popleft()
+            num_queue.append(temp)
+print(*list(num_queue),sep="\n")
