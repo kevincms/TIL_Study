3c095ba0aba0cd96abcb5689c821022b730df430
“kevincms” <kevincms314@naver.com>
2024-03-24 08:04:41 +0900

    스택 연결리스트 이용해서 구현

--- Baekjoon/Baekjoon_step_problem/18_스택/1_스택/1. 스택.py
+++ Baekjoon/Baekjoon_step_problem/18_스택/1_스택/1. 스택.py
@@ -1,15 +1,15 @@
 '''
 문제
- -정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
+ -정수를 저장하는 스택을 구현한 다음, 입력으로 주어지는 명령을 처리하는 프로그램을 작성하시오.
 명령은 총 다섯 가지이다.
 
 push X: 정수 X를 스택에 넣는 연산이다.
-pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 size: 스택에 들어있는 정수의 개수를 출력한다.
 empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
-top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 입력
- -첫째 줄에 주어지는 명령의 수 N (1 ≤ N ≤ 10,000)이 주어진다. 둘째 줄부터 N개의 줄에는 명령이 하나씩 주어진다. 주어지는 정수는 1보다 크거나 같고, 100,000보다 작거나 같다. 문제에 나와있지 않은 명령이 주어지는 경우는 없다.
+ -첫째 줄에 주어지는 명령의 수 N (1 ≤ N ≤ 10,000)이 주어진다. 둘째 줄부터 N개의 줄에는 명령이 하나씩 주어진다. 주어지는 정수는 1보다 크거나 같고, 100,000보다 작거나 같다. 문제에 나와있지 않은 명령이 주어지는 경우는 없다.
 출력
  -출력해야하는 명령이 주어질 때마다, 한 줄에 하나씩 출력한다.
 예제 입력 1
@@ -62,3 +62,47 @@ pop
 
 https://www.acmicpc.net//problem/10828
 '''
+
+class linklist():
+    def __init__(self,data,next) -> None:
+        self.data=data
+        self.next=next
+
+class stack():
+    def __init__(self) -> None:
+        self.Top=None
+        self.Size=0
+
+    def push(self,data):
+        node=linklist(data,self.Top)
+        self.Top=node
+        self.Size+=1
+
+    def pop(self):
+        if self.empty(): return -1
+        else:
+            e=self.Top
+            self.Top=e.next
+            self.Size-=1
+            return e.data
+    
+    def size(self): return self.Size
+    def empty(self): return int(self.Top==None)
+    def top(self):
+        if self.empty(): return -1
+        else: return self.Top.data
+
+import sys
+input=sys.stdin.readline
+input_t=int(input())
+S=stack()
+for i in range(input_t):
+    input_cmd=list(map(str,input().split()))
+    if len(input_cmd)==1:
+        cmd=input_cmd[0]
+        if cmd=="pop": print(S.pop())
+        elif cmd=="size": print(S.size())
+        elif cmd=="empty": print(S.empty())
+        else: print(S.top())
+    else:
+        S.push(input_cmd[1])
