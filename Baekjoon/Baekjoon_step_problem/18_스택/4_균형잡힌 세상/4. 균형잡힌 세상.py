83f784d23146148cd034c1cc567b5d62ef4f0700
“kevincms” <kevincms314@naver.com>
2024-03-27 10:10:01 +0900

    괄호 쌍이 맞는지 확인하는 문제 [대괄호 추가한 버전] - 스택 이용

--- Baekjoon/Baekjoon_step_problem/18_스택/4_균형잡힌 세상/4. 균형잡힌 세상.py
+++ Baekjoon/Baekjoon_step_problem/18_스택/4_균형잡힌 세상/4. 균형잡힌 세상.py
@@ -14,7 +14,49 @@
 입력
  -하나 또는 여러줄에 걸쳐서 문자열이 주어진다. 각 문자열은 영문 알파벳, 공백, 소괄호("( )") 대괄호("[ ]")등으로 이루어져 있으며, 길이는 100글자보다 작거나 같다.
 입력의 종료조건으로 맨 마지막에 점 하나(".")가 들어온다.
+
+So when I die (the [first] I will see in (heaven) is a score list).
+[ first in ] ( first out ).
+Half Moon tonight (At least it is better than no Moon at all].
+A rope may form )( a trail in a maze.
+Help( I[m being held prisoner in a fortune cookie factory)].
+([ (([( [ ] ) ( ) (( ))] )) ]).
+ .
+.
+
 출력
  -각 줄마다 해당 문자열이 균형을 이루고 있으면 "yes"를, 아니면 "no"를 출력한다.
+
+yes
+yes
+no
+no
+no
+yes
+yes
+
 https://www.acmicpc.net//problem/4949
 '''
+
+from collections import deque
+import sys
+input=sys.stdin.readline
+end=False
+stack=deque()
+while(1):
+    input_str=input().rstrip()
+    if input_str==".": break
+    for i in input_str:
+        if i=="(" or i=="[": stack.append(i)
+        elif i==")" or i=="]":
+            if len(stack)==0:
+                stack.append(i)
+                break
+            else:
+                temp=stack.pop()
+                if (temp=="[" and i==")") or (temp=="(" and i=="]"):
+                    stack.append(i)
+                    break
+    if len(stack)==0: print("yes")
+    else: print("no")
+    stack.clear()
