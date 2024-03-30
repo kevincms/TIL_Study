1422b57bd9333eaecc5950e659a937fdb9c96c43
“kevincms” <kevincms314@naver.com>
2024-03-30 11:06:24 +0900

    동적할당 푸는 방법 정리 + 6. 정수 삼각형 해결

--- Baekjoon/Baekjoon_step_problem/15_동적 계획법 1/6_정수 삼각형/6. 정수 삼각형.py
+++ Baekjoon/Baekjoon_step_problem/15_동적 계획법 1/6_정수 삼각형/6. 정수 삼각형.py
@@ -1,9 +1,14 @@
 '''
 문제
- -7
-      3   8
-    8   1   0
-  2   7   4   4
+ -
+        7
+
+      3   8
+
+    8   1   0
+
+  2   7   4   4
+
 4   5   2   6   5
 위 그림은 크기가 5인 정수 삼각형의 한 모습이다.
 맨 위층 7부터 시작해서 아래에 있는 수 중 하나를 선택하여 아래층으로 내려올 때, 이제까지 선택된 수의 합이 최대가 되는 경로를 구하는 프로그램을 작성하라. 아래층에 있는 수는 현재 층에서 선택된 수의 대각선 왼쪽 또는 대각선 오른쪽에 있는 것 중에서만 선택할 수 있다.
@@ -11,17 +16,38 @@
 입력
  -첫째 줄에 삼각형의 크기 n(1 ≤ n ≤ 500)이 주어지고, 둘째 줄부터 n+1번째 줄까지 정수 삼각형이 주어진다.
 출력
- -첫째 줄에 합이 최대가 되는 경로에 있는 수의 합을 출력한다.
+ -첫째 줄에 합이 최대가 되는 경로에 있는 수의 합을 출력한다.
 예제 입력 1
- -5
-7
-3 8
-8 1 0
-2 7 4 4
-4 5 2 6 5
+ -5
+7
+3 8
+8 1 0
+2 7 4 4
+4 5 2 6 5
+
 
 예제 출력 1
- -30
+ -30
+
 
 https://www.acmicpc.net//problem/1932
+
+big o 
+2초 4천만 = 4*10^7
+삼격형 크기 n -> n^2 n 최대값이 500이기 때문에 충분
 '''
+import sys
+input=sys.stdin.readline
+tri_size=int(input())
+tri_list=[]
+sum_list=[None]*tri_size
+
+tri_list=list(map(int,input().split()))
+sum_list[0]=tri_list[0]
+for t in range(1,tri_size):
+    tri_list=list(map(int,input().split()))
+    tri_list[0]+=sum_list[0]
+    for i in range(1,t): tri_list[i]+=max(sum_list[i-1],sum_list[i])
+    tri_list[-1]+=sum_list[t-1]
+    for i in range(t+1): sum_list[i]=tri_list[i]
+print(max(sum_list))
