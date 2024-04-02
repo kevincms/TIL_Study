daf448f156b768db9443780b49c423c1a14e4ce3
“kevincms” <kevincms314@naver.com>
2024-04-02 15:01:12 +0900

    연결리스트 큐 시간초과. 떄문에 원형 큐로 미리 메모리 2백만개 할당해서 구현

--- Baekjoon/Baekjoon_step_problem/19_큐, 덱/1_큐 2/1. 큐 2.py
+++ Baekjoon/Baekjoon_step_problem/19_큐, 덱/1_큐 2/1. 큐 2.py
@@ -47,6 +47,62 @@ front
 
 https://www.acmicpc.net//problem/18258
 '''
+
+class Cir_Queue():
+    def __init__(self) -> None:
+        self.Front=0
+        self.rear=0
+        self.capacity=2000000
+        self.queue=[None]*self.capacity
+        self.Size=0
+
+    def empty(self):
+        return self.Front==self.rear
+    
+    def full(self):
+        return self.Front==(self.rear+1)%self.capacity
+    
+    def push(self,data):
+        self.queue[self.rear]=data
+        self.rear=(self.rear+1)%self.capacity
+        self.Size+=1
+
+    def pop(self):
+        if self.empty(): return -1
+        e=self.queue[self.Front]
+        self.Front=(self.Front+1)%self.capacity
+        self.Size-=1
+        return e
+    
+    def front(self):
+        if self.empty(): return -1
+        else: return self.queue[self.Front]
+
+    def back(self):
+        if self.empty(): return -1
+        else: return self.queue[(self.rear-1+self.capacity)%self.capacity]
+
+    def size(self):
+        return self.Size
+    
+import sys
+input=sys.stdin.readline
+input_t=int(input())
+Q=Cir_Queue()
+for i in range(input_t):
+    input_cmd=list(map(str,input().split()))
+    if len(input_cmd)==1:
+        cmd=input_cmd[0]
+        if cmd=="pop": print(Q.pop())
+        elif cmd=="size": print(Q.size())
+        elif cmd=="empty": print(int(Q.empty()))
+        elif cmd=="front": print(Q.front())
+        else: print(Q.back())
+    else:
+        Q.push(input_cmd[1])
+
+
+""" 연결리스트
 class linklist():
     def __init__(self,data,next) -> None:
         self.data=data
@@ -59,7 +115,7 @@ class Queue():
         self.Size=0
 
     def empty(self):
-        return int(self.Front==None)
+        return self.Front==None
     
     def push(self,data):
         node=linklist(data,None)
@@ -74,7 +130,7 @@ class Queue():
     def pop(self):
         if self.empty(): return -1
         e=self.Front
-        if(self.Front==self.rear):
+        if(self.Size==1):
             self.Front=None
             self.rear=None
         else:
@@ -103,19 +159,9 @@ for i in range(input_t):
         cmd=input_cmd[0]
         if cmd=="pop": print(Q.pop())
         elif cmd=="size": print(Q.size())
-        elif cmd=="empty": print(Q.empty())
+        elif cmd=="empty": print(int(Q.empty()))
         elif cmd=="front": print(Q.front())
         else: print(Q.back())
     else:
         Q.push(input_cmd[1])
-
-
-
-'''
-push X: 정수 X를 큐에 넣는 연산이다.
-pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-size: 큐에 들어있는 정수의 개수를 출력한다.
-empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
-front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
-'''
+"""
