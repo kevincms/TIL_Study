e3dfe0fa98e21428d4adfb418a147d760795511f
“kevincms” <kevincms314@naver.com>
2024-04-16 23:41:49 +0900

    c++ 연결리스트 스택 구현 실패. push 이후 값을 호출할때 값이 변경되는 문제발생

--- Baekjoon/Baekjoon_step_problem/18_스택/1_스택/18_1.cpp
+++ Baekjoon/Baekjoon_step_problem/18_스택/1_스택/18_1.cpp
@@ -62,3 +62,72 @@ pop
 
 https://www.acmicpc.net//problem/10828
 */
+
+#include <iostream>
+using namespace std;
+
+class linked_list{
+    friend class Stack;
+private:
+    
+    linked_list *next;
+public:
+    int data;
+    linked_list(int data, linked_list *next){
+        this->data=data;
+        this->next=next;
+    }
+};
+
+class Stack{
+private:
+    int size;
+    
+public:
+    linked_list *Top;
+    Stack(){size=0;Top=NULL;}
+    void push(int data);
+    int pop();
+    int Size(){return size;}
+    bool empty(){return Top==NULL;}
+    int top();
+};
+
+int main(int argc, char const *argv[])
+{
+    Stack stack;
+    cout<<stack.empty()<<endl;
+    cout<<stack.Size()<<endl;
+    stack.push(5);
+    cout<<stack.Size()<<endl;
+    cout<<stack.Top->data<<endl;
+    cout<<stack.top()<<endl;
+    return 0;
+}
+/*
+push X: 정수 X를 스택에 넣는 연산이다.
+pop: 스택에서 가장 위에 있는 정수를 빼고, 그 수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+size: 스택에 들어있는 정수의 개수를 출력한다.
+empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
+top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+*/
+
+void Stack::push(int data){
+    linked_list node(data,NULL);
+    node.next=Top;
+    Top=&node;
+    size++;
+}
+
+int Stack::pop(){
+    if(empty()) return -1;
+    int temp=(*Top).data;
+    Top=(*Top).next;
+    size--;
+    return temp;
+}
+
+int Stack::top(){
+    if(empty()) return -1;
+    return Top->data;
+}
