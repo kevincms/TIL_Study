96a2bd23d83b81a894c761b5be8876b943b20905
“kevincms” <kevincms314@naver.com>
2024-07-15 06:12:18 +0900

    그래프 루프에 연결된 노드 개수 구하는 문제 재귀 DFS로 다시 구현

--- Baekjoon/solved/class_3/2606-바이러스.py
+++ Baekjoon/solved/class_3/2606-바이러스.py
@@ -1,6 +1,27 @@
 import sys
 from collections import deque
 
+def count_DFS(start):
+    if visited[start]: return 0
+    visited[start]=True
+    count=0
+    for i in com[start]:
+        if not visited[i]: count+=count_DFS(i)+1
+    return count
+
+input=sys.stdin.readline
+N=int(input())
+com=[[] for _ in range(N+1)]
+visited=[False]*(N+1)
+M=int(input())
+for _ in range(M):
+    a, b=map(int,input().split())
+    com[a].append(b)
+    com[b].append(a)
+print(count_DFS(1))
+
+'''
+- BFS queue로 구현
 def count_BFS(start):
     count=0
     record=[]
@@ -15,12 +36,4 @@ def count_BFS(start):
                 queue.append(i)
     return count
 
-input=sys.stdin.readline
-N=int(input())
-com=[[] for _ in range(N+1)]
-M=int(input())
-for _ in range(M):
-    a, b=map(int,input().split())
-    com[a].append(b)
-    com[b].append(a)
-print(count_BFS(1))
+'''
